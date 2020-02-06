58ec0ffcb788250371d4f9efc1537e0ad5f7b224
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-02-06 12:04:07 -0600

    I update settings

--- backend_user/settings.py
+++ backend_user/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3pbw^%pp15lxg*ofqucq44df825pc$__a0f&%wyxa-6coz19jd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
